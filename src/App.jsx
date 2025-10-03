663fafac419e2be4a665cb36a3a3c4291e7794c8
Camilo <camilolon6@gmail.com>
2025-10-02 20:39:11 -0500

    Corrijo rutas de imágenes para producción

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import {
 } from "lucide-react";
 
 import "./Portfolio.css";
-import remoteShellImg from "./assets/images/remote-shell.png";
 const Portfolio = () => {
   const [activeSection, setActiveSection] = useState("home");
 
@@ -45,7 +44,7 @@ const Portfolio = () => {
       title: "Administrador remoto multiplataforma",
       description:
         "Aplicación desarrollada en GoLang que permite a un computador que ejecuta un sistema operativo Windows conectarse mediante sockets y ejecutar comandos de consola en otro equipo con sistema Linux.",
-      image: remoteShellImg,
+      image: "/Portafolio/images/remote-shell.png",
       technologies: ["Go", "Shell", "Sockets"],
       github: "https://github.com/CamiloLVZ/RemoteShellClient",
       demo: null,
